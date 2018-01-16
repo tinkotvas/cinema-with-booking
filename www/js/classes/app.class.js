23ec68d146a1d4db52a105cda477f4094bb50974
huisusu <yudasuhui@hotmail.com>
2018-01-16 17:52:42 +0100

    load movies JSON file

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -3,20 +3,25 @@ class App {
   constructor(){
 
     // Tell jsonflex to recreate instances of the class Garment
-    JSON._classes(Garment);
+    JSON._classes(Film);
 
     // Load garments, add as a property, then start the app
-    JSON._load('garments').then((garments)=>{
-      this.garments = garments;
-      this.start();
+    JSON._load('movies').then((movies)=>{
+      this.film = movies;
+      //this.start();
+
+      //test code. check if JSON load into the right way
+      for(let f of this.film){
+        console.log(f.getTitle());
+      }
     });
 
   }
 
-  start(){
-    // Empty main element, then render auditoriums to main
-    $('main').empty();
-    this.garments.render('main');
-  }
+  // start(){
+  //   // Empty main element, then render auditoriums to main
+  //   $('main').empty();
+  //   this.garments.render('main');
+  // }
 
 }
