e4fba0fdd819da08a53359ce1372845cc27b1c06
huisusu <yudasuhui@hotmail.com>
2018-01-31 10:16:18 +0100

    moviepage

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -80,10 +80,8 @@ class App extends Base {
     }
     if (url == '/filmer') {
       $('main').empty();
-        let moviepage=new MoviePage();
-        moviepage.render('main');
       this.render('main', 'moviepage')
-  
+
 
       let list = new List();
       list.loadJSON(() => list.renderMovies(), "movies");
