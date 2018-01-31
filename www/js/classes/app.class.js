d39aa069a21fff2db645ec44c084a20a24c314b3
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-01-31 10:10:27 +0100

    moviepage funkar

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -71,8 +71,9 @@ class App extends Base{
     }
     if (url == '/filmer') {
       $('main').empty();
-      let moviepage=new MoviePage();
-      moviepage.render('main');
+      this.render('main', 'moviepage')
+      //let moviepage=new MoviePage();
+      //moviepage.render('main');
 
       let list = new List();
       list.loadJSON(() => list.renderMovies(), "movies");
