00ce25ac164421c6a0312a7fa20ebb84e6ce0f31
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-02-01 14:06:22 +0100

    Add and subtract tickets + price

--- www/js/classes/booking.class.js
+++ www/js/classes/booking.class.js
@@ -1,3 +1,7 @@
 class Booking extends Base{
     
+
+	
+
+
 }
--- www/js/classes/modal.class.js
+++ www/js/classes/modal.class.js
@@ -8,6 +8,10 @@ class Modal extends Base{
       this.toggleInfoModal();
       this.idBtn;
       this.indexToOpen;
+      this.confirmBooking();
+      //this.addTickets();
+      //this.subTickets();
+      this.eventHandler();
   }
 
   toggleBookingModal(){
@@ -46,4 +50,103 @@ class Modal extends Base{
       //$('#'+idBtn).modal('toggle');
     });
   }
+
+  confirmBooking(){
+    $(document).on('click', '.confirm-booking', function() {
+      alert('hej din get')
+    });
+  }
+
+  eventHandler(){
+    let adultTickets = 0;
+    let childTickets = 0;
+    let seniorTickets = 0;
+    $(document).on('click', '#add-adult, #add-child, #add-senior, #sub-adult, #sub-child, #sub-senior', function(){
+      let id = event.target.id;
+      if(id == 'add-adult'){
+        adultTickets++;
+        $('.ticketArea').removeClass('d-none');
+        $('#adultTickets').removeClass('d-none');
+        $('#adultTickets').text('Vuxen: ' + adultTickets + ' ');
+      }
+      else if(id == 'add-child'){
+        childTickets++;
+        $('.ticketArea').removeClass('d-none');
+        $('#childTickets').removeClass('d-none');
+        $('#childTickets').text('Barn: ' + childTickets + ' ');
+      }
+      else if(id == 'add-senior'){
+        seniorTickets++;
+        $('.ticketArea').removeClass('d-none');
+        $('#seniorTickets').removeClass('d-none');
+        $('#seniorTickets').text('Senior: ' + seniorTickets);
+      }
+      else if(id == 'sub-adult'){
+        if(adultTickets == 0){
+        return;
+      }
+        adultTickets--;
+        if(adultTickets == 0){
+          $('#adultTickets').addClass('d-none');
+        }
+        $('#adultTickets').text('Vuxen: ' + adultTickets + ' ');
+      }
+      else if(id == 'sub-child'){
+        if(childTickets == 0){
+        return;
+      }
+        childTickets--;
+        if(childTickets == 0){
+          $('#childTickets').addClass('d-none');
+        }
+        $('#childTickets').text('Barn: ' + childTickets + ' ');
+      }
+      else if(id == 'sub-senior'){
+        if(seniorTickets == 0){
+        return;
+      }
+        seniorTickets--;
+        if(seniorTickets == 0){
+          $('#seniorTickets').addClass('d-none');
+        }
+        $('#seniorTickets').text('Senior: ' + seniorTickets + ' ');
+      }
+      if(seniorTickets == 0 && adultTickets == 0 && childTickets == 0){
+        $('.ticketArea').addClass('d-none');
+      }
+      let totalPrice = childTickets * 55 + adultTickets * 95 + seniorTickets * 65;
+      $('.total-price').text('Summa: ' + totalPrice);
+    })
+  }
+
+/*addTickets(){
+    
+    $(document).on('click', '#add-adult', function() {
+      tickets++;
+      $('.adult').removeClass('d-none');
+      $('#adultTickets').text(tickets);
+    });
+
+  }*/
+
+  /*subTickets(){
+    $(document).on('click', '#sub-adult', function() {
+      //let tickets = $('#adultTickets').text();
+      if(tickets == 0){
+        return;
+      }
+      tickets--;
+      if(tickets < 1){
+        $('.adult').addClass('d-none');
+      }
+      //console.log(tickets);
+      $('#adultTickets').text(tickets);
+      /*tickets++;
+      $('.adult').removeClass('d-none');
+      $('#adultTickets').text(tickets);
+    });
+
+  }
+*/
+
 }
