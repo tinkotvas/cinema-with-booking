0861c29f1cfbec59c068b9ca51588d7ffdcdb885
André Tente <iamandretente@gmail.com>
2018-02-02 22:28:25 +0100

    sorting old and active bookings

--- www/js/classes/mainpage.class.js
+++ www/js/classes/mainpage.class.js
@@ -6,23 +6,11 @@ class MainPage extends Base{
     }
     renderMain(){
       this.index = 0;
-      // let date = new Date();
-      // let month = date.getMonth() + 1;
-      // if (month < 10) {
-      //     month = "0" + month;
-      // }
-      // let day = date.getDate();
-      // if (day < 10) {
-      //     day = "0" + day;
-      // }
-      // let dateString = `${date.getFullYear()}-${month}-${day}`;
-      console.log('day');
+
+
       this.render('main', 1);
       for (let film of this.films) {
         $('.carousel-item').first().addClass('active');
-        if (true) {
-
-        }
         this.render('.banner', 2);
         this.index++;
       }
--- www/js/classes/mypage.class.js
+++ www/js/classes/mypage.class.js
@@ -8,10 +8,26 @@ class MyPage extends Base {
   }
 
   renderBooking(){
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    let todayDate = date.getFullYear()+month+day+date.getHours()+date.getMinutes();
     this.render('main', 1);
     this.index = 0;
+
     for (let booking of this.dummyBooking) {
-      this.render('.activeBooking', 2);
+      let bookingDate = booking.date.replace(/-/g, '')+booking.time.replace(/\./g, '')
+      if (bookingDate > todayDate) {
+        this.render('.activeBooking', 2);
+      }
+      else
+      this.render('.pastBooking', 2);
       this.index++;
     }
   }
