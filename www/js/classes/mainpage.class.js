eec8f972c93b6a2fb801dee9d8e89c53cb162255
André Tente <iamandretente@gmail.com>
2018-02-13 12:51:00 +0100

    new layout

--- www/js/classes/mainpage.class.js
+++ www/js/classes/mainpage.class.js
@@ -2,16 +2,41 @@ class MainPage extends Base{
     constructor(films){
       super();
       this.films = films;
-      this.renderCarousel();
-    }
-
-    renderCarousel(){
       this.index = 0;
+      this.renderCarousel();
+      this.renderCarouselItem();
+      this.eventHandler();
+    }
+    eventHandler(){ 
+      let that = this;
+      $(document).on('click', '.gallery-next', function() {
+        that.index < (that.films.length-1) ? that.index++ : that.index = 0;
+        $('.gallery').empty();
+        that.renderCarouselItem();
+        that.animateItemIn();
+      });
+      $(document).on('click', '.gallery-previous', function() {
+        console.log(that.index);
+        
+        that.index < 1 ? that.index = (that.films.length-1) : that.index-- ;
+        that.renderCarouselItem();
+      });
+    }
+    renderCarousel(){
       this.render('main', 1);
-      for (let film of this.films) {
-        $('.carousel-item').first().addClass('active');
-        this.render('.banner', 2);
-        this.index++;
-      }
+    }
+    renderCarouselItem(){
+      this.render('.gallery', 2);
+    }
+    animateItemIn(){
+      $('.header-film').css({
+        width: '0%'
+      });
+      $('.header-film').animate({
+        width: '100%'
+      });
+      $('.gallery-image-container').animate({
+        height: '100%'
+      });
     }
 }
