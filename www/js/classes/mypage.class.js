cf6a8a3aa3693583cfc712bfad11ac6ab51ef8df
huisusu <yudasuhui@hotmail.com>
2018-02-14 19:32:39 +0100

    fixed!

--- www/js/classes/mypage.class.js
+++ www/js/classes/mypage.class.js
@@ -4,13 +4,13 @@ class MyPage extends Base {
     this.render('main', 1);
     this.bookings = [];
     this.toggleOrderModal();
-    this.films=films;
-   
+    this.films = films;
+
   }
   init(jsonName) {
     return JSON._load(jsonName)
-      .then((data) => { 
-        this.bookings= data.bookingHistory;
+      .then((data) => {
+        this.bookings = data.bookingHistory;
         this.sortBooking(this.bookings);
       });
   }
@@ -35,7 +35,7 @@ class MyPage extends Base {
     //   day = "0" + day;
     // }
     // let todayDate = date.getFullYear() + month + day + date.getHours() + date.getMinutes();
-    
+
     this.index = 0;
 
     for (let booking of this.bookings) {
@@ -50,29 +50,26 @@ class MyPage extends Base {
       this.index++;
     }
   }
-  
-  toggleOrderModal(){
+
+  toggleOrderModal() {
     let that = this;
-    $(document).on("click", '.mypage-item', function() {
+    $(document).on("click", '.mypage-item', function () {
       that.idBtn = $(this).attr('id');
       let index = 0;
       let indexPoster = 0;
-      // might be something wrong with the loops, 
-      //it runs many times and undifined for the first 3 loops that.indexToOpenPoster is not correct
       for (let booking of that.bookings) {
-        if ('orderModalToggle'+booking.filmTitle == that.idBtn) {
+        if ('orderModalToggle' + booking.filmTitle == that.idBtn) {
           $('.modal-container-item').empty();
           that.indexToOpen = index;
-          for (let film of that.films) {
-            console.log(film.title);
-            if(film.title == booking.filmTitle ){
-              that.indexToOpenPoster = indexPoster;
-            }
-            indexPoster++;
-          }
         }
         index++;
-          console.log(that.indexToOpenPoster);
+      }
+
+      for (let film of that.films) {
+        if ('orderModalToggle' + film.title == that.idBtn) {
+          that.indexToOpenPoster = indexPoster;
+        }
+        indexPoster++;
       }
       that.render('.modal-container-item', 3);
       $('#orderModal').modal('toggle');
@@ -80,5 +77,3 @@ class MyPage extends Base {
   }
 }
 
-// something went wrong for bookingModal when link from InfoModal
-//It's not scrolling and auditorium render is not currect
