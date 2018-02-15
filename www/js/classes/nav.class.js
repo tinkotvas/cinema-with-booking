b06939b056edb93b73ac21d488219bb82eba4ab6
huisusu <yudasuhui@hotmail.com>
2018-02-15 10:47:06 +0100

    add back from conflix

--- www/js/classes/nav.class.js
+++ www/js/classes/nav.class.js
@@ -2,7 +2,14 @@ class Nav extends Base {
 
   constructor(currentUser, films, viewings) {
     super();
+    this.navCollapse();
   }
-  
 
+  navCollapse(){
+    $(document).ready(function () {
+      $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+      });
+    });
+  }
 }
